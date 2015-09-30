4be00cd36dbe4b56de426795bec5e65993d31b5e
Toan Tran <ttkbnt@hotmail.com>
2015-09-30 15:06:39 -0500

    Code cleanup
    - Removed more stuff from .gitignore
    - Uninstalled and removed use of some npm packages
    - Deleted /server/www
    - Added a gulpfile

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 ﻿var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
-//var bodyParser = require('body-parser');
+var debug = require('debug')('Dilemma.Web');
 
 //var routes = require('./routes/index');
 //var users = require('./routes/users');
 
 var app = express();
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function () {
+    console.log('i should log a debug message');
+    debug('Express server listening on port ' + server.address().port);
+});
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-//app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
-//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname));
 
@@ -57,6 +53,8 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
+
+
 
 
 module.exports = app;
---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require('gulp');
+
+gulp.task('log', function () {
+    console.log('hello gulp!')
+});
+
+gulp.task('copy', function () {
+    // copies the angular file into the scripts folder
+    gulp.src('node_modules/angular/angular.js')
+        .pipe(gulp.dest('public/scripts'));
+    
+})
