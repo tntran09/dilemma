fb703e3b24d670562eaba340fdbd70f28832d0fe
Toan Tran <ttkbnt@hotmail.com>
2015-10-27 17:13:47 -0500

    Separated controllers
    - Created a new controller for each view. Placed controller-specific
    properties into appropriate controllers
    - Changed page title per view
    - Explicitly declared controller controller scopes to avoid ambiguity
    - Added real validation to newQuery input

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -10,14 +10,37 @@
         templateUrl: '/views/splash.html'
     });
 })
-.controller('mainCtrl', function ($scope, $location) {
-    $scope.pageTitle = 'DILEMMA';
-    
+.controller('mainCtrl', function ($scope, $location, $rootScope) {
     $scope.dilemma = {
-        query: ''
+        query: '',
+        factors: []
     };
     
     $scope.onSplash = function () {
         return $location.path() === "";
     }
+})
+.controller('splashCtrl', function ($scope, $rootScope) {
+    $rootScope.pageTitle = 'DILEMMA';
+})
+.controller('newCtrl', function ($scope, $rootScope, $location) {
+    var newCtrl = this;
+    var attemptedSubmission = false;
+
+    $rootScope.pageTitle = 'NEW';
+
+    newCtrl.showNewQueryError = function () {
+        return newCtrl.newForm.query.$error.required && (attemptedSubmission || newCtrl.newForm.query.$dirty);
+    };
+    
+    newCtrl.submitNewQuery = function () {
+        attemptedSubmission = true;
+        
+        if (newCtrl.newForm.$valid) {
+            $location.path('/factors');
+        }
+    }
+})
+.controller('factorsCtrl', function ($scope, $rootScope) {
+    $rootScope.pageTitle = 'FACTORS';
 });
