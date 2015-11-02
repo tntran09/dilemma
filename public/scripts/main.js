2dd3615093598ffcc793f70b3765d8738ef633ad
Toan Tran <ttkbnt@hotmail.com>
2015-11-02 14:57:37 -0600

    Fix naming
    - Normalize some function/property names
    - Remove leading slash from templateUrl's in attempt to fix routing
    issues from gh-pages
    - Change hyperlink to button to trigger angular function
    - Add Visual Studio artifacts to .gitignore

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,22 +1,22 @@
 ﻿angular.module('gutCheck', ['ngRoute'])
 .config(function ($routeProvider) {
     $routeProvider.when('/new', {
-        templateUrl: '/views/new.html'
+        templateUrl: 'views/new.html'
     })
     .when('/factors', {
-        templateUrl: '/views/factors.html'
+        templateUrl: 'views/factors.html'
     })
     .otherwise({
-        templateUrl: '/views/splash.html'
+        templateUrl: 'views/splash.html'
     });
 })
 .factory('dataSvc', function () {
-    var _pageTitle = ''
+    var _pageTitle = '';
     
     return {
         get pageTitle() { return _pageTitle; },
         set pageTitle(t) {
-            if (t === null || typeof (t) === 'string') {
+            if (angular.isString(t)) {
                 _pageTitle = t;
             }
         }
@@ -25,14 +25,28 @@
 .controller('mainCtrl', function ($location, dataSvc) {
     var mainCtrl = this;
 
-    mainCtrl.pageTitle = dataSvc.__lookupGetter__('pageTitle');
+    mainCtrl.getPageTitle = dataSvc.__lookupGetter__('pageTitle');
     
-    mainCtrl.onSplash = function () {
+    mainCtrl.isOnSplash = function () {
         return $location.path() === "";
     }
+
+    mainCtrl.getBackUrl = function () {
+        switch ($location.path()) {
+            case '/new': return '#';
+            case '/factors': return '#/new';
+            default: return '#';
+        }
+    }
 })
-.controller('splashCtrl', function (dataSvc) {
+.controller('splashCtrl', function ($location, dataSvc) {
+    var splashCtrl = this;
+
     dataSvc.pageTitle = 'DILEMMA';
+
+    splashCtrl.goNext = function () {
+        $location.path('/new');
+    }
 })
 .controller('newCtrl', function ($location, dataSvc) {
     var newCtrl = this;
